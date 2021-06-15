f8e69bafb7d4e9de0039901aa7599df4f50d3a01
Balazs Kegl <balazs.kegl@gmail.com>
2021-06-15 21:03:50 +0200

    import ClassifierBaseScoreType

--- external_imports/utils/score_types.py
+++ external_imports/utils/score_types.py
@@ -1,16 +1,7 @@
 import numpy as np
 from sklearn.metrics import (
     average_precision_score, precision_recall_curve, f1_score)
-from rampwf.score_types import BaseScoreType
-
-
-class ClassifierBaseScoreType(BaseScoreType):
-    def score_function(self, ground_truths, predictions):
-        self.label_names = ground_truths.label_names
-        y_pred_label_index = predictions.y_pred_label_index
-        y_true_label_index = ground_truths.y_pred_label_index
-        self.check_y_pred_dimensions(y_true_label_index, y_pred_label_index)
-        return self.__call__(y_true_label_index, y_pred_label_index)
+from rampwf.score_types import ClassifierBaseScoreType, BaseScoreType
 
 
 class F1_score(ClassifierBaseScoreType):
