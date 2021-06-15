787d235e1501998bca03f29feae5d7ddf463625c
Roman Yurchak <rth.yurchak@gmail.com>
2021-06-15 13:48:55 +0200

    Fix utils sys.path for ramp-board

--- problem.py
+++ problem.py
@@ -4,6 +4,10 @@ import os
 import joblib
 import numpy as np
 import rampwf as rw
+import sys
+from pathlib import Path
+
+sys.path.insert(1, str(Path(__file__).parent / 'external_imports'))
 
 import utils
 
