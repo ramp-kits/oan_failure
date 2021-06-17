1981a93f3a9f54c19cfe45256085e672e8e581d9
Aladin Virmaux <aladin.virmaux@huawei.com>
2021-06-17 22:23:50 +0800

    FIX unecessary copy

--- external_imports/utils/workflow.py
+++ external_imports/utils/workflow.py
@@ -21,9 +21,7 @@ class FeatureExtractor(object):
         return fe
 
     def test_submission(self, trained_model, X):
-        # We copy here otherwise when X is not sliced (in predict), no copy is
-        # created, and the first CV fold overwrites the original target data.
-        return trained_model.transform(X.copy())
+        return trained_model.transform(X)
 
 
 class Classifier(object):
